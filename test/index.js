c842322c78cf6aea005149a6a84f1161650864c6
Rongxin Zhang <rongxin@rets.ly>
2015-11-03 10:56:10 -0800

    Fix tests

--- test/index.js
+++ test/index.js
@@ -1,36 +1,26 @@
-
-/**
- * Dependencies
- */
-var assert = require('assert');
-var Retsly = require('retsly-js-backbone');
-
 /**
  * Tests
  */
-suite('Retsly');
+describe("Retsly Backbone", function () {
+  it('Has loaded', function () {
+    expect(Retsly).to.exist;
+  })
 
-test('Retsly has models', function () {
-  assert('object' == typeof Retsly.Models);
+  it('Has Model', function () {
+    expect(Retsly.Model).to.exist;
+  })
+
+  it('Has Collection', function () {
+    expect(Retsly.Collection).to.exist;
+  })
+
+  it('Model#url()', function () {
+    var mock = {
+      get: function(){return 'query'},
+      options: {urlBase:'/api/v1'},
+      fragment: 'listings',
+      vendorID: 'test'
+    };
+    expect(Retsly.Model.prototype.url.call(mock)).to.equal('/api/v1/test/listings/query');
+  });
 })
-
-test('Retsly has collections', function () {
-  assert('object' == typeof Retsly.Collections);
-});
-
-test('Retsly has Section', function () {
-  assert('function' == typeof Retsly.Section);
-});
-
-
-suite('Retsly.Model');
-
-test('Model#url()', function () {
-  var mock = {
-    get: function(){return 'qux'},
-    options: {urlBase:'/foo'},
-    fragment: 'bar',
-    vendorID: 'baz'
-  };
-  assert(Retsly.Model.prototype.url.call(mock) === '/foo/bar/baz/qux');
-});
